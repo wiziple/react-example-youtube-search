e6a3e21b07cd1f1adf59e1b8deb95b9eee47daa9
wiziple <wiziple@gmail.com>
2017-06-04 21:10:21 -0700

    input fixed #section 1, lecture 19

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,16 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term : ''};
+    this.state = { term : 'initial value'};
   }
 
   render() {
     // this.state.term = event.target.value; // BAD
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
+        <input 
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value })} />
         Value of the input : { this.state.term } 
       </div>
     );
