e3497691697a4b80ce82bd9deccef661bea4575c
wiziple <wiziple@gmail.com>
2017-06-04 21:00:44 -0700

    added state example

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term : ''};
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    // this.state.term = event.target.value; // BAD
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input : { this.state.term } 
+      </div>
+    );
   }
 }
 
