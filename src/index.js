030a96895629d6e1e7a40274286fe43bb6d66cb4
wiziple <wiziple@gmail.com>
2017-06-04 17:54:27 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+
+import SearchBar from './component/search_bar';
+
+
+const API_KEY = 'AIzaSyDwct3zLgV1cyYs1EVrc2hJCnMCKGaTsR4';
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
