b2322b06f94c7269fd2da1db2bc5de2677a3359f
wiziple <wiziple@gmail.com>
2017-06-04 20:47:58 -0700

    changed function component to class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input /> //React.createElement
-};
+class SearchBar extends Component {
+  render() {
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+}
 
 export default SearchBar;
