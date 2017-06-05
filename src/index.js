874625e70e24c3db1b80b02cea5a6dc20c81e7d1
wiziple <wiziple@gmail.com>
2017-06-04 20:45:19 -0700

    added input to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
 
-import SearchBar from './component/search_bar';
+import SearchBar from './components/search_bar';
 
 
 const API_KEY = 'AIzaSyDwct3zLgV1cyYs1EVrc2hJCnMCKGaTsR4';
 
 const App = () => {
-    return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 
